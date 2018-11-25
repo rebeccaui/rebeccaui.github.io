8ca08a1663910160297d9397109e5670315b3749
rwieberdink <rebeccawieberdink@gmail.com>
2018-11-25 03:18:00 -0500

    footer, navbar, fonts, portfolio changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,19 @@
-// Get the current year for the copyright
-$('#year').text(new Date().getFullYear());
+$(document).ready(function() {
+    //When the DOM is ready
+    $(".navbar a").click(function(){
+        $("body,html").animate({
+         scrollTop:$("#" + $(this).data('value')).offset().top
+        },1000)
+        
+    });
+
+    $(".footer a").click(function(){
+        $("body,html").animate({
+         scrollBottom:$("#" + $(this).data('value')).offset().top
+        },1000)
+        
+    });
+
+    // Get the current year for the copyright
+    $('#year').text(new Date().getFullYear());
+});    
